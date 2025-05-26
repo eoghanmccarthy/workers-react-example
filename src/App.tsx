643581e5810ff297209433a57ac23920b73cdf98
Eoghan McCarthy <eoghanmccarthy@outlook.com>
2025-05-26 17:41:46 +0900

    Config wrangler

--- src/App.tsx
+++ src/App.tsx
@@ -48,9 +48,9 @@ function App() {
           Edit <code>worker/index.ts</code> to change the name
         </p>
       </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
+      {/*<p className='read-the-docs'>*/}
+      {/*  Click on the Vite and React logos to learn more*/}
+      {/*</p>*/}
     </>
   )
 }
