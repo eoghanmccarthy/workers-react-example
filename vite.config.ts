ef014fbf53f18845849e2cad10ffc2145bf55b43
Eoghan McCarthy <eoghanmccarthy@outlook.com>
2025-05-31 23:50:20 +0900

    Add tanstack router

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflare()],
+  plugins: [TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),react(), cloudflare()],
 })
