21aedb451548bda9628dc112a42de05392cf79ab
Eoghan McCarthy <eoghanmccarthy@outlook.com>
2025-06-01 12:43:40 +0900

    Configure router

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,15 @@ import tseslint from 'typescript-eslint'
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended,
+        // ...tseslint.configs.recommended,
+        // Remove ...tseslint.configs.recommended and replace with this
+        ...tseslint.configs.recommendedTypeChecked,
+        // Alternatively, use this for stricter rules
+        // ...tseslint.configs.strictTypeChecked,
+        // Optionally, add this for stylistic rules
+        ...tseslint.configs.stylisticTypeChecked
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
